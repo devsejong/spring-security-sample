8363b765cca0a72159c03de45c776459364dc941
SejongPark <devsejong@gmail.com>
2017-06-24 12:19:41 +0900

    polish

--- src/main/java/net/chandol/study/config/SecurityConfig.java
+++ src/main/java/net/chandol/study/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package net.chandol.study.config;
 import net.chandol.study.security.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,6 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,14 +32,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .permitAll()
                 .and()
+            .userDetailsService(userService)
             .csrf()
                 .disable();
         //@formatter:on
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth, UserService userService) throws Exception {
-        auth.userDetailsService(userService);
     }
 
 }
